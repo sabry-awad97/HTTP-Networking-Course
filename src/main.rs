3d707c48d628c2047e3d61d50ce5add0d942c03b
Sabry Awad <dr.sabry1997@gmail.com>
2023-04-08 17:55:46 +0200

    URIs and URLs

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,20 @@
-use serde::Deserialize;
 use std::error::Error;
 
-#[derive(Debug)]
-struct DnsResolver {
-    client: reqwest::Client,
+use url::Url;
+
+fn get_port_number(url: &str) -> Option<u16> {
+    let parsed_url = Url::parse(url).ok()?;
+    let port = match parsed_url.port_or_known_default() {
+        Some(port) => port,
+        None => return None,
+    };
+    Some(port)
 }
-
-impl DnsResolver {
-    fn new() -> DnsResolver {
-        DnsResolver {
-            client: reqwest::Client::new(),
-        }
-    }
-
-    async fn resolve(&self, domain: &str) -> Result<Option<String>, Box<dyn Error>> {
-        let url = format!(
-            "https://cloudflare-dns.com/dns-query?name={}&type=A",
-            domain
-        );
-
-        let resp = self
-            .client
-            .get(&url)
-            .header("accept", "application/dns-json")
-            .send()
-            .await?
-            .json::<DNSResponse>()
-            .await?;
-        let answer = resp.answer;
-        let ip_address = answer.iter().find_map(|r| {
-            if r.record_type == 1 {
-                Some(r.data.clone())
-            } else {
-                None
-            }
-        });
-
-        Ok(ip_address)
-    }
+fn set_port_number(url: &str, port: u16) -> Result<String, Box<dyn Error>> {
+    let mut parsed_url = Url::parse(url)?;
+    parsed_url
+        .set_port(Some(port))
+        .map_err(|_| "cannot be base")?;
+    Ok(parsed_url.to_string())
 }
-
-#[derive(Debug, Deserialize)]
-struct DNSRecord {
-    #[serde(rename = "type")]
-    record_type: u8,
-    data: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct DNSResponse {
-    #[serde(rename = "Answer")]
-    answer: Vec<DNSRecord>,
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let domain = "api.boot.dev";
-    let dns_resolver = DnsResolver::new();
-    let ip_address = dns_resolver.resolve(domain).await?;
-
-    match ip_address {
-        Some(ip) => println!("Found IP address for domain {}: {}", domain, ip),
-        None => println!("No IP address found for domain {}", domain),
-    }
-    Ok(())
-}
+fn main() {}
